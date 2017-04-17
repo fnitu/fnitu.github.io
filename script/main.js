84e4b68f82982986f8b6fee9cabdd1732cddfce0
fnitu <fnitu@users.noreply.github.com>
2017-04-17 13:25:28 +0300

    add event handling example

--- script/main.js
+++ script/main.js
@@ -1,5 +1,19 @@
-function helloWorld(){
-    console.log ("Alive!");
-}
+document.addEventListener("DOMContentLoaded", init);
 
-helloWorld();
+function init(event) {
+    console.log("DOM Content loaded!");
+
+    //All my initialization code goes here
+    var element = document.getElementById("coolParagraph");
+
+    element.addEventListener("click", paragraphClick);
+    element.addEventListener("mouseover", paragraphMouseOver);
+};
+
+function paragraphClick(event) {
+    console.log("Help! They are clicking me!")
+};
+
+function paragraphMouseOver(event) {
+    console.log("Help! They are mousing me!")
+};
